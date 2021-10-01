4318886d2a0cdb3b81c32531f7828fb4b2fcda2c
Denisa Haka <denisa.haka@ymail.com>
2021-10-01 22:49:46 +0100

    added API integration

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
 import React from 'react';
 
+import Weather from './Weather';
 
-import SearchEngine from './SearchEngine';
-import Temperature from './Temperature';
-import './styles.css';
+
+
+import './App.css';
+
 
 
  export default function App() {
   return (
-    <div className="WeatherApp">
-      <div class="container">
-        <div class="weather-app">
-          <SearchEngine />
-          <h1>Sydney NSW, Australia</h1>
-          <ul>
-            <li>Wednesday 22:00</li>
-            <li>Partly Cloudy</li>
-          </ul>
-          <div class="row">
-            <div class="col">
-             
-                <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                  alt="Cloudy icon"
-                  class="weather-icon"
-                /> </div>
-                <div class="col">
-                   <Temperature />
-                </div>
+    <div className="App">
+      <div className="container">
         
-            <div class="col-6">
-              <ul>
-                <li>Precipitation: 0%</li>
-                <li>Humidity: 77%</li>
-                <li>Wind: 8 km/h</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-       <p class="text-center">
-          <a
+         
+         <Weather defaultCity="London" />
+          
+            
+          
+        
+       <footer>
+         This project was coded by{" "} 
+         <a href="https://github.com/Deni-ux" class="text-decoration-none">Denisa Haka </a> 
+          {" "}and is {" "} <a
             href="https://github.com/Deni-ux/weather-react"
             class="text-decoration-none opensource "
-            >Open-source</a> code 
-          by <a href="https://github.com/Deni-ux" class="text-decoration-none">Denisa Haka </a> 
-        </p>
+            >open-sourced on GitHub</a>
+        </footer>
       </div>
-      
-    </div>
+      </div>
+    
   );
 }
 
---
+++ src/Weather.js
@@ -0,0 +1,105 @@
+import React , {useState} from "react";
+
+import axios from "axios";
+
+import "./Weather.css";
+
+export default function Weather(props){
+//boolean used with a conditional rendering
+ 
+
+  //create a new state with weather data which is going to be a  whole object - by default an empty one, that is going to set all the info we want
+  const [weather, setWeather] = useState({loaded: false})
+  //this is an object that has a key false by default
+function handleResponse(response){
+  
+  setWeather({
+    loaded: true,
+    temperature:response.data.main.temp,
+    humidity:response.data.main.humidity,
+    wind: response.data.wind.speed,
+    date: "Friday 12:00",
+    city: response.data.name,
+    description:response.data.weather[0].description,
+    iconUrl:"https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+    
+  });
+}
+
+if (weather.loaded) {
+   return(
+        <div className="Weather">
+ <form class="search-form">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                autoFocus="on"
+                autoComplete="off"
+                className="form-control shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control btn btn-primary shadow-sm"
+              />
+            </div>
+          </div>
+        </form>
+             <h1>{weather.city}</h1>
+          <ul>
+            <li>{weather.date}</li>
+            <li className="text-capitalize">{weather.description}</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+             
+                <img
+                  src={weather.iconUrl}
+                  alt={weather.description}
+                  className=" "
+                />
+                
+                   <span className="temperature" id="temperature">
+           {Math.round(weather.temperature)}
+          </span>
+          <span className="units">
+            <a href="https://" id="celsius-link">
+              °C
+            </a>{" "}
+            |
+            <a href="https://" id="fahrenheit-link">
+              °F
+            </a>
+          </span>
+                 </div>  
+               
+                <div className="col-6">
+              <ul className="ms-5">
+                
+                <li>Humidity: {weather.humidity}%</li>
+                <li>Wind: {Math.round(weather.wind)}km/h</li>
+              </ul>
+            </div>
+        </div>
+</div>
+        
+    );
+} else {
+  const apiKey = "b2694a5d8f39bb351277f910bc5d27c4";
+  
+ const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(handleResponse);
+
+return "Loading..";
+//use a loading spinner
+
+}
+
+  
+   
+}
