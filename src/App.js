475936542844f3020ec54234f911167215b462d8
Denisa Haka <denisa.haka@ymail.com>
2021-10-01 12:59:21 +0100

    added a simple search form using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
+
+import SearchEngine from './SearchEngine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <form class="search-form">
-        <div class="row">
-          <div class="col-9">
-            <input 
-            type="search"
-            placeholder="City name"
-            autoFocus={true}
-              autocomplete="off"
-                class="form-control shadow-sm"
-            />
-          </div>
-          <div class="col-3"></div>
-        </div>
-      </form>
-    
+      <div class="container">
+      <div class="weather-app">
+        <SearchEngine />
+
+
+        </div></div>
+      
     </div>
   );
 }
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,3 +1,32 @@
 import React from "react";
 
 import "./styles.css";
+
+export default class SearchEngine extends React.Component {
+  render = () => {
+    return (
+      <div className="WeatherApp">
+        <form class="search-form">
+          <div class="row">
+            <div class="col-9">
+              <input
+                type="search"
+                placeholder="Type a city.."
+                autofocus="on"
+                autocomplete="off"
+                class="form-control shadow-sm"
+              />
+            </div>
+            <div class="col-3">
+              <input
+                type="submit"
+                value="Search"
+                class="form-control btn btn-primary shadow-sm"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  };
+}
